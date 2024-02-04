69f9a82f943f1ba183ec6fa083280659061890b5
Vara986 <95453994+Vara986@users.noreply.github.com>
2024-02-04 21:46:14 +0530

    Add files via upload

---
+++ javascr.js
@@ -0,0 +1,6 @@
+function showPopup(planName) {
+    alert('You toggled the price for ' + planName);
+    document.body.style.backgroundColor = "#f8f8f8";
+  document.body.style.color = "#333";
+  }
+  
